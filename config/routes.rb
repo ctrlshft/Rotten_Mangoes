75678fb1d21e3a5ab82259a185fb7fdf6816917f
ctrlshft <julian.rogochevsky@gmail.com>
2015-11-17 01:36:48 +0000

    11

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
 
-  get 'sessions/create'
-
-resources :sessions, only: [:new, :create]
+resources :sessions, only: [:new, :create, :destroy]
 resources :movies
 resources :users, only: [:new, :create]
 
